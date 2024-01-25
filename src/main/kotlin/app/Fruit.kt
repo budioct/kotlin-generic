f9e0fa0d9fbf9b3e4ed24a7309ae5b0c49c51abb
Budhi Octaviansyah <budioct@gmail.com>
2024-01-25 18:07:10 +0700

    comparable

---
+++ src/main/kotlin/app/Fruit.kt
@@ -0,0 +1,27 @@
+package app
+
+class Fruit(val name: String, val quantity: Int): Comparable<Fruit>{
+
+    /**
+     * Comparable
+     * ● Sebelumnya kita sudah tahu bahwa operator perbandingan == dan != akan menggunakan metode
+     *   equals sebagai implementasinya.
+     * ● Bagaimana dengan operator perbandingan lainnya? Seperti > >= < <= ?
+     * ● Operator perbandingan tersebut bisa kita lakukan, jika object kita mewariskan interface generic Comparable
+     */
+
+    override fun compareTo(other: Fruit): Int {
+        return quantity.compareTo(other.quantity) // compareTo() method extends // equals object
+    } // method implement
+
+}
+
+fun main() {
+
+    val apel = Fruit("Apel", 5000)
+    val mangga = Fruit("Mangga", 7000)
+
+    println(apel > mangga)
+    println(apel < mangga)
+
+}
